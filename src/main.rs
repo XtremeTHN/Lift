1a53b74908a652917b67546e2ca57b1a8b20087b
Axel <nigthmaresans2@gmail.com>
2026-03-01 17:45:57 -0600

    refactor: common code transformed into a func

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use positioned_io::ReadAt;
 use roms::fs::pfs::PartitionFs;
 use std::{
     fs::File,
-    io::{Read, Write},
+    io::{Read, Seek, Write},
 };
 
 use crate::roms::{
@@ -16,7 +16,11 @@ use crate::roms::{
         nca::{self, Nca},
         xci::Xci,
     },
-    fs::{hfs::HFSEntry, pfs::PFSEntry, romfs::RomFs},
+    fs::{
+        hfs::HFSEntry,
+        pfs::{PFSEntry, PFSHeader, PartitionFsHeader},
+        romfs::RomFs,
+    },
     keyring::Keyring,
 };
 
@@ -34,7 +38,47 @@ fn protocol() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn nxroms() {
+fn list_romfs_files(rom_fs: RomFs) {
+    info!("Listing romfs files...");
+    for (index, file) in rom_fs.files.iter().enumerate() {
+        let name = String::from_utf8(file.name.clone()).expect("error while decoding name");
+        info!("{}: {}", index, name);
+    }
+}
+
+fn get_control_nca_romfs<T: BinRead + PFSHeader, R: ReadAt + Read + Seek>(
+    pfs: PartitionFs<T>,
+    part: R,
+) -> Option<RomFs> {
+    let mut keyring = Keyring::new();
+    keyring.parse().expect("error while parsing keyring");
+    for (index, entry) in pfs.header.entry_table().iter().enumerate() {
+        let name = pfs.get_name_for_entry(entry).expect("failed to get name:");
+
+        let mut r = pfs.open_entry(entry, &part);
+
+        if name.split(".").collect::<Vec<&str>>()[1] != "nca" {
+            continue;
+        }
+
+        let mut nca = Nca::new(&keyring, &mut r).expect("err");
+
+        match nca.header.content_type {
+            nca::ContentType::Control => {
+                info!("found control nca at index {}: {}", index, name);
+                let mut fs = nca.open_fs(0, &mut r).expect("err");
+                let rom_fs = RomFs::new(&mut fs).expect("err");
+
+                return Some(rom_fs);
+            }
+            _ => {}
+        }
+    }
+
+    None
+}
+
+fn xci_test() {
     let mut file = File::open("ori.xci").expect("er");
     let mut xci = Xci::new(&mut file).expect("err");
 
@@ -45,76 +89,26 @@ fn nxroms() {
 
     info!("Listing pfs files...");
 
-    let mut keyring = Keyring::new();
-    keyring.parse().expect("");
-    for (index, entry) in pfs.header.entry_table.iter().enumerate() {
-        let name = pfs.get_name_for_entry(entry).expect("failed to get name:");
-        info!("{}: {}", index, name);
+    let romfs = get_control_nca_romfs(pfs, part).unwrap();
 
-        let mut r = pfs.open_entry(entry, &mut part);
+    list_romfs_files(romfs);
+}
 
-        let mut nca = Nca::new(keyring.clone(), &mut r).expect("err");
+fn nsp_test() {
+    let mut file = File::open("celeste.nsp").expect("failed");
+    let pfs = PartitionFs::new_default_header(&mut file).expect("failed");
 
-        match nca.header.content_type {
-            nca::ContentType::Control => {
-                info!("found control nca at index {}: {}", index, name);
-                let mut fs = nca.open_fs(0, &mut r).expect("err");
-                let rom_fs = RomFs::new(&mut fs).expect("err");
-                // info!("{:?}", rom_fs);
+    let romfs = get_control_nca_romfs(pfs, file).unwrap();
+    list_romfs_files(romfs);
+}
 
-                info!("Listing romfs files...");
-                for file in rom_fs.files.iter() {
-                    let mut f = rom_fs.get_file(file, &mut fs);
-                    let mut buf = vec![0u8; file.size as usize];
-                    f.read_exact(&mut buf).expect("err");
-
-                    let mut out = File::create(String::from_utf8(file.name.clone()).expect("err"))
-                        .expect("err");
-                    out.write_all(&buf).expect("err");
-                }
-
-                break;
-            }
-            _ => {}
-        }
-    }
-
-    // let entry = pfs.header.entry_table[6];
-    // let mut range = pfs.open_entry(&entry, file);
-
-    // let mut keyring = Keyring::new();
-    // keyring.parse().expect("coulnd't parse");
-
-    // let mut nca = nca::Nca::new(keyring, &mut range).expect("Nca error");
-
-    // info!("dumping");
-
-    // let mut fs = nca.open_fs(0, &range).expect("fail");
-    // let rom_fs = RomFs::new(&mut fs).expect("err");
-    // info!("{:?}", rom_fs);
-
-    // let mut f = rom_fs.get_file(&rom_fs.files[0], &fs);
-
-    // let mut buf = vec![0u8; enc_r.inner.size as usize];
-    // let mut f = File::create("out.bin").expect("fata");
-    // enc_r.read(&mut buf).expect("");
-
-    // f.write_all(&buf).expect("fatal");
-
-    // info!("Has rights id?: {:?}", nca.header.rights_id);
-    // info!(
-    //     "Nca {} have a content type of {:?}",
-    //     pfs.header.get_name_for_entry(&entry).expect(""),
-    //     nca.header.content_type
-    // );
-
-    // info!("Key Area: {:?}", hex::encode(nca.key_area.aes_ctr_key));
-    // info!("Entries: {:?}", nca.header.fs_entries);
-    // info!("Headers: {:?}", nca.fs_headers);
+fn nxroms() {
+    // xci_test();
+    nsp_test();
 }
 
 fn main() {
-    let env = Env::default().filter_or("LIFT_LOG", "trace");
+    let env = Env::default().filter_or("LIFT_LOG", "info");
 
     env_logger::init_from_env(env);
 
