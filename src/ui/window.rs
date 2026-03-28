9ccdd338440375ec387d56e1ad18274860dfd47b
Axel <nigthmaresans2@gmail.com>
2026-03-28 12:45:08 -0600

    feat: utils

--- src/ui/window.rs
+++ src/ui/window.rs
@@ -1,11 +1,13 @@
-use gtk::prelude::*;
 use adw::subclass::prelude::*;
+use gtk::prelude::*;
 use gtk::{gio, glib};
 
 use super::home_page::HomePage;
 use super::roms_page::RomsPage;
 
 mod imp {
+    use crate::finder::Finder;
+
     use super::*;
 
     #[derive(Debug, Default, gtk::CompositeTemplate)]
@@ -14,7 +16,9 @@ mod imp {
         #[template_child]
         pub toast: TemplateChild<adw::ToastOverlay>,
         #[template_child]
-        pub navigation: TemplateChild<adw::NavigationView>
+        pub navigation: TemplateChild<adw::NavigationView>,
+
+        pub finder: Finder,
     }
 
     #[glib::object_subclass]
@@ -28,6 +32,18 @@ mod imp {
             RomsPage::ensure_type();
 
             klass.bind_template();
+
+            klass.install_action_async("win.start-finder", None, async |page, _, _| {
+                page.imp().finder.start(
+                    |bc| {
+                        // log::info!("connected");
+                    },
+                    || {
+                        log::info!("disconnected");
+                    },
+                    page.clone(),
+                );
+            });
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -40,6 +56,8 @@ mod imp {
     impl WindowImpl for LiftWindow {}
     impl ApplicationWindowImpl for LiftWindow {}
     impl AdwApplicationWindowImpl for LiftWindow {}
+
+    impl LiftWindow {}
 }
 
 glib::wrapper! {
---
+++ src/utils.rs
@@ -0,0 +1,10 @@
+use gtk::{
+    glib::{object::IsA, variant::ToVariant},
+    prelude::WidgetExt,
+};
+
+pub fn send_error<W: IsA<gtk::Widget>>(widget: &W, message: &str) {
+    widget
+        .activate_action("win.toast", Some(&message.to_string().to_variant()))
+        .expect("toast");
+}
